28f8136fb46b03b10cbf364609bf6a10402e2578
Miriam Völkl <miriamvoelkl@gmail.com>
2024-01-23 16:34:24 +0100

    add random character to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,26 @@
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data: randomCharacter, isLoading } = useSWR(
+    "/api/random-character",
+    fetcher
+  );
+  if (isLoading) {
+    return <h1>...loading....</h1>;
+  }
+
+  return (
+    <>
+      <h1>
+        Hi, my name is {randomCharacter.firstName} {randomCharacter.lastName}{" "}
+        ⭐️
+      </h1>
+      <p>
+        I am {randomCharacter.age} years old and my pet is a{" "}
+        {randomCharacter.pet}.
+      </p>
+    </>
+  );
 }
